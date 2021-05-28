4f5359337ffd58f38f39fd33099a6f1df4631ad9
harry <harrytasker999@gmail.com>
2021-05-28 17:55:55 +1200

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,1 @@
-$('textarea').each(function () {
-  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-}).on('input', function () {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
-});
-
-
 CKEDITOR.replace( 'editor' );
